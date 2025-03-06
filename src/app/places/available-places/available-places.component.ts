fd2ab1a1e2c068b92ec67fb113bb15cdd3351f35
Kapil Jugnu <kr_kapil@hotmail.com>
2025-03-06 12:53:14 +0530

    fetching fallback state

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -15,10 +15,12 @@ import { map } from 'rxjs';
 })
 export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false);
   private httpClient = inject(HttpClient);
   private destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
+    this.isFetching.set(true);
     const subscription = this.httpClient
       .get<{ places: Place[] }>('http://localhost:3000/places')
       .pipe(map(resData => resData.places))
@@ -26,6 +28,9 @@ export class AvailablePlacesComponent implements OnInit {
         next: (places) => {
           this.places.set(places)
         },
+        complete: () => {
+          this.isFetching.set(false)
+        }
       });
 
     this.destroyRef.onDestroy(() => {

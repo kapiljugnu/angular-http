476c59b160c7bd246e3b309bf187b0f023b9e224
Kapil Jugnu <kr_kapil@hotmail.com>
2025-03-07 17:17:58 +0530

    http interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
 
+function addHeaders(request: HttpRequest<unknown>, next: HttpHandlerFn) {
+    const req = request.clone({
+        // headers: request.headers.set('X-DEBUG', 'Testing')
+    })
+    return next(req);
+}
+
+function loggingInterceptor(request: HttpRequest<unknown>, next: HttpHandlerFn) {
+    console.log('Outgoing request');
+    console.log(request)
+    return next(request);
+}
 
 bootstrapApplication(AppComponent, {
-    providers: [provideHttpClient()]
+    providers: [provideHttpClient(
+        withInterceptors([addHeaders, loggingInterceptor])
+    )]
 }).catch((err) => console.error(err));

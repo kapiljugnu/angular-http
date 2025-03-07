9873cd1cc71ebdca0c4b6d6cd13982aa886b31b6
Kapil Jugnu <kr_kapil@hotmail.com>
2025-03-07 17:23:40 +0530

    using rxjs operator in interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HttpEventType, HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 function addHeaders(request: HttpRequest<unknown>, next: HttpHandlerFn) {
     const req = request.clone({
@@ -13,7 +14,15 @@ function addHeaders(request: HttpRequest<unknown>, next: HttpHandlerFn) {
 function loggingInterceptor(request: HttpRequest<unknown>, next: HttpHandlerFn) {
     console.log('Outgoing request');
     console.log(request)
-    return next(request);
+    return next(request).pipe(tap({
+        next: event => {
+            if (event.type === HttpEventType.Response) {
+                console.log('[Incoming Response]');
+                console.log(event.status)
+                console.log(event.body)
+            }
+        }
+    }));
 }
 
 bootstrapApplication(AppComponent, {
